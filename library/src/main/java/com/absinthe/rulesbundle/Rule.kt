6687675e80d4a85e017d71748070d2a56ede16a7
Absinthe <zhaobozhen2025@gmail.com>
2024-02-08 13:58:31 +0800

    Parcelize `Rule` entity.

--- library/src/main/java/com/absinthe/rulesbundle/Rule.kt
+++ library/src/main/java/com/absinthe/rulesbundle/Rule.kt
@@ -1,11 +1,14 @@
 package com.absinthe.rulesbundle
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Rule(
     val label: String,
     @DrawableRes val iconRes: Int,
     val descriptionUrl: String?,
     val regexName: String?,
     val isSimpleColorIcon: Boolean
-)
+) : Parcelable
